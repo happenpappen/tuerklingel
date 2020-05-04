994ab3def7684a074c734e9d5a746f46eff4bce6
Christoph Stoppe <happenpappen@gmail.com>
2020-05-04 21:55:11 +0200

    Reformatted source code.

--- src/tuerklingel.cpp
+++ src/tuerklingel.cpp
@@ -17,8 +17,8 @@ DST dst;
 dst_limit_t beginning;
 dst_limit_t end;
 
-int button_pressed=1;
-String lastVisitDate="";
+int button_pressed = 1;
+String lastVisitDate = "";
 String myID = System.deviceID();
 
 #define ONE_DAY_MILLIS (24 * 60 * 60 * 1000)
@@ -41,24 +41,27 @@ MQTT client(MQTT_HOST, 1883, mqtt_callback);
 
 ApplicationWatchdog wd(60000, System.reset);
 
-void mqtt_callback(char *topic, byte *payload, unsigned int length) {
+void mqtt_callback(char *topic, byte *payload, unsigned int length)
+{
     String myTopic = String(topic);
 
     bool stateChanged = false;
     char *myPayload = NULL;
 
-    myPayload = (char *) malloc(length + 1);
+    myPayload = (char *)malloc(length + 1);
 
     memcpy(myPayload, payload, length);
     myPayload[length] = 0;
 
-    if (!client.isConnected()) {
+    if (!client.isConnected())
+    {
         client.connect(myID, MQTT_USER, MQTT_PASSWORD);
     }
 
     client.publish("/" + myID + "/state/LastPayload", String(myPayload));
 
-    if (stateChanged) {
+    if (stateChanged)
+    {
         publishState();
     }
 
@@ -66,32 +69,39 @@ void mqtt_callback(char *topic, byte *payload, unsigned int length) {
     myPayload = NULL;
 }
 
-void publishState() {
+void publishState()
+{
 
-    if (!client.isConnected()) {
+    if (!client.isConnected())
+    {
         client.connect(myID, MQTT_USER, MQTT_PASSWORD);
     }
 
-    if (client.isConnected()) {
+    if (client.isConnected())
+    {
         client.publish("/" + myID + "/state/FirmwareVersion", System.version());
     }
 }
 
-void publishButtonPushed() {
+void publishButtonPushed()
+{
 
     lastVisitDate = Time.format(Time.now(), TIME_FORMAT_ISO8601_FULL);
-    
-    if (!client.isConnected()) {
+
+    if (!client.isConnected())
+    {
         client.connect(myID, MQTT_USER, MQTT_PASSWORD);
     }
 
-    if (client.isConnected()) {
+    if (client.isConnected())
+    {
         client.publish("/" + myID + "/state/Visitor", "True");
         client.publish("/" + myID + "/state/VisitorDate", lastVisitDate);
     }
 }
 
-void loadSettings() {
+void loadSettings()
+{
 
     uint16_t maxSize = EEPROM.length();
     uint16_t address = 1;
@@ -100,14 +110,18 @@ void loadSettings() {
 
     EEPROM.get(address++, version);
 
-    if (version == SETTINGS_MAGIC) { // Valid settings in EEPROM?
-// FIXME       EEPROM.get(address, dispMode);
-    } else {
+    if (version == SETTINGS_MAGIC)
+    {   // Valid settings in EEPROM?
+        // FIXME       EEPROM.get(address, dispMode);
+    }
+    else
+    {
         saveSettings();
     }
 }
 
-void saveSettings() {
+void saveSettings()
+{
 
     uint16_t address = SETTINGS_START;
     uint16_t maxlength = EEPROM.length();
@@ -116,52 +130,59 @@ void saveSettings() {
     EEPROM.write(address++, SETTINGS_MAGIC);
 }
 
-void playMelody() {
-  if (myDFPlayer.readState() != 513) {
-    myDFPlayer.play(1);
-  }
+void playMelody()
+{
+    if (myDFPlayer.readState() != 513)
+    {
+        myDFPlayer.play(1);
+    }
 }
 
-void setup() {
-  Serial.begin(9600);
-  Serial1.begin(9600);
-  delay(2000);
-  Serial.println();
-  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+void setup()
+{
+    Serial.begin(9600);
+    Serial1.begin(9600);
+    delay(2000);
+    Serial.println();
+    Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
 
-  if (!myDFPlayer.begin(Serial1)) {  //Use softwareSerial to communicate with mp3.
-    Serial.println(F("Unable to begin:"));
-    Serial.println(F("1.Please recheck the connection!"));
-    Serial.println(F("2.Please insert the SD card!"));
-    while (true);
-  }
-  Serial.println(F("DFPlayer Mini online."));
+    if (!myDFPlayer.begin(Serial1))
+    { //Use softwareSerial to communicate with mp3.
+        Serial.println(F("Unable to begin:"));
+        Serial.println(F("1.Please recheck the connection!"));
+        Serial.println(F("2.Please insert the SD card!"));
+    }
+    else
+    {
+        Serial.println(F("DFPlayer Mini online."));
+    }
 
-  //----Set serial communictaion time out 500ms
-  myDFPlayer.setTimeOut(500);
+    //----Set serial communictaion time out 500ms
+    myDFPlayer.setTimeOut(500);
 
-  //----Set volume----
-  myDFPlayer.volume(30);  //Set volume value (0~30).
+    //----Set volume----
+    myDFPlayer.volume(30); //Set volume value (0~30).
 
-  //----Set different EQ----
-  //myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
-  myDFPlayer.EQ(DFPLAYER_EQ_POP);
+    //----Set different EQ----
+    //myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
+    myDFPlayer.EQ(DFPLAYER_EQ_POP);
 
-  //----Set device we use SD as default----
-  myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
+    //----Set device we use SD as default----
+    myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
 
-  //----Init Pushbutton pin----
-  pinMode(PIN_BTN, INPUT_PULLUP);
-  
+    //----Init Pushbutton pin----
+    pinMode(PIN_BTN, INPUT_PULLUP);
 }
 
-void loop() {
+void loop()
+{
     button_pressed = digitalRead(PIN_BTN);
     Serial.print(F("Button read: "));
     Serial.println(button_pressed);
-    if (button_pressed == LOW) {
+    if (button_pressed == LOW)
+    {
         Serial.println(F("Button pushed!"));
-        button_pressed=HIGH;
+        button_pressed = HIGH;
         publishButtonPushed();
         playMelody();
     }
--- src/tuerklingel.h
+++ src/tuerklingel.h
@@ -1,3 +1,6 @@
 #ifndef __TUERKLINGEL_H__
 #define __TUERKLINGEL_H__
+
+// Insert definitions here:
+
 #endif
